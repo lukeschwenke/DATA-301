0340500d1b6faed61ab3feeafe4c889395205a61
Luke Schwenke <lmschwenke@email.wm.edu>
2018-10-03 14:47:39 -0400

    Update Lab 4 work.py

--- Lab 4 work.py
+++ Lab 4 work.py
@@ -1,29 +1,11 @@
-
-# coding: utf-8
-
-# In[14]:
-
-
 import pandas as pd
 data = pd.read_csv("./data.csv")
-
-
-# In[15]:
-
 
 data.shape
 list(data)
 
-
-# In[16]:
-
-
 X = data.iloc[:,2:4]
 y = data.iloc[:,1]
-
-
-# In[17]:
-
 
 X = X.values
 y = y.values
@@ -31,50 +13,17 @@ y = y.values
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-
-# In[18]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state = 1693)
 scale_X = StandardScaler()
 X_train = scale_X.fit_transform(X_train)
 X_test = scale_X.transform(X_test)
 
-
-# In[19]:
-
-
 #Logistic Regression
-
 from sklearn.linear_model import LogisticRegression
-
-
-# In[20]:
-
-
 logistic_classifier = LogisticRegression(random_state = 1693)
-
-
-# In[21]:
-
-
 logistic_classifier.fit(X_train, y_train)
-
-
-# In[22]:
-
-
 y_pred = logistic_classifier.predict(X_test)
-
-
-# In[23]:
-
-
 y_pred
-
-
-# In[24]:
-
 
 # PREDICTION FOR LOG 
 log_x = [[15.78, 17.89]]
@@ -82,40 +31,14 @@ scaled_log_x = scale_X.transform(log_x)
 log_x_prediction = logistic_classifier.predict_proba(scaled_log_x)
 log_x_prediction
 
-
-# In[25]:
-
-
 y_test
 
-
-# In[26]:
-
-
 y_pred_probabilities = logistic_classifier.predict_proba(X_test)
-
-
-# In[27]:
-
-
 y_pred_probabilities
 
-
-# In[28]:
-
-
 from sklearn.metrics import confusion_matrix
-
-
-# In[29]:
-
-
 confMat = confusion_matrix(y_test, y_pred)
 confMat
-
-
-# In[30]:
-
 
 def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=None):
     """pretty print for confusion matrixes"""
@@ -140,10 +63,6 @@ def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=
             print(cell, end=" ")
         print()
 
-
-# In[31]:
-
-
 print_cm(confMat, ["Benign", "Malignant"])
 
 #75 cases we predicted benign, and it actually was benign
@@ -151,40 +70,14 @@ print_cm(confMat, ["Benign", "Malignant"])
 #6 cases we predicted malignant, and it was actually benign (False Positive)
 #10 cases we predicted benign, and it was actually malignant (False Negative)
 
-
-# In[32]:
-
-
 # KNN
 from sklearn.neighbors import KNeighborsClassifier
 
-
-# In[33]:
-
-
 nn_classifier = KNeighborsClassifier(n_neighbors = 5, metric = "minkowski", p=2)
-
-
-# In[34]:
-
-
 nn_classifier.fit(X_train, y_train)
 
-
-# In[35]:
-
-
 y_pred_NN = nn_classifier.predict(X_test)
-
-
-# In[36]:
-
-
 y_pred_NN
-
-
-# In[37]:
-
 
 # PREDICTION FOR KNN
 knn_x = [[17.18, 8.65]]
@@ -192,22 +85,10 @@ scaled_knn_x = scale_X.transform(knn_x)
 knn_x_prediction = nn_classifier.predict(scaled_knn_x)
 knn_x_prediction
 
-
-# In[38]:
-
-
 y_test
-
-
-# In[39]:
-
 
 confMat_NN = confusion_matrix(y_test, y_pred_NN)
 print_cm(confMat_NN, ["Benign", "Malignant"])
-
-
-# In[40]:
-
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -238,37 +119,17 @@ def viz_cm(model, labels):
     plt.legend()
     plt.show()
 
-
-# In[41]:
-
-
 viz_cm(model = nn_classifier, labels = ["Radius Mean", "Texture Mean"])
-
-
-# In[42]:
-
 
 #SVM 
 from sklearn.svm import SVC
 svc_classifier = SVC(kernel="linear", random_state=1693)
 svc_classifier.fit(X_train, y_train)
 
-
-# In[43]:
-
-
 y_pred_SVC = svc_classifier.predict(X_test)
-
-
-# In[44]:
-
 
 confMat_SVC = confusion_matrix(y_test, y_pred_SVC)
 print_cm(confMat_SVC, ["Benign", "Malignant"])
-
-
-# In[45]:
-
 
 # PREDICTION FOR SVM
 svm_x = [[15.78, 17.89]]
@@ -276,37 +137,17 @@ scaled_svm_x = scale_X.transform(svm_x)
 svm_x_prediction = svc_classifier.predict(scaled_svm_x)
 svm_x_prediction
 
-
-# In[46]:
-
-
 viz_cm(model = svc_classifier, labels = ["Radius Mean", "Texture Mean"])
 #Simpler model (SVC) is doing better than KNN since we have 16 false neg/pos in this one vs. 19 in the other.
-
-
-# In[47]:
-
 
 from sklearn.svm import SVC
 kernelSVC_classifier = SVC(kernel="rbf", random_state=1693)
 kernelSVC_classifier.fit(X_train, y_train)
 
-
-# In[48]:
-
-
 y_pred_SVC_kernel = kernelSVC_classifier.predict(X_test)
-
-
-# In[49]:
-
 
 confMat_SVC_kernel = confusion_matrix(y_test, y_pred_SVC_kernel)
 print_cm(confMat_SVC_kernel, ["Benign", "Malignant"])
-
-
-# In[50]:
-
 
 viz_cm(model = svc_classifier, labels = ["Radius Mean", "Texture Mean"])
 
